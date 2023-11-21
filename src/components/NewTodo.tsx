0f768c7657d1720ac25684d7d9af5605bf9e1b06
Felix W <felixwillem01@yahoo.com>
2023-11-20 20:30:30 -0500

    Added more styling to list

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,10 +1,12 @@
 import React, { useRef } from "react";
 
+import "./NewTodo.css";
+
 type NewTodoProps = {
-    onAddTodo: (todoText: string) => void
+  onAddTodo: (todoText: string) => void;
 };
 
-const NewTodo: React.FC<NewTodoProps> = props => {
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -13,15 +15,13 @@ const NewTodo: React.FC<NewTodoProps> = props => {
   };
 
   return (
-    <>
-      <form onSubmit={todoSubmitHandler}>
-        <div>
-          <label htmlFor="todo-text">Todo Text</label>
-          <input type="text" id="todo-text" ref={textInputRef} />
-        </div>
-        <button type="submit">ADD TODO</button>
-      </form>
-    </>
+    <form onSubmit={todoSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./TodoList.css";
+
 // you have to be specific when passing props
 interface TodoListProps {
   items: { id: string; text: string }[];
@@ -9,16 +11,14 @@ interface TodoListProps {
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
-      <li>
-        {props.items.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
-            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
-              DELETE
-            </button>
-          </li>
-        ))}
-      </li>
+      {props.items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
